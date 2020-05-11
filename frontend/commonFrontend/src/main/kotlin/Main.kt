63218169761b9f00773733257cb504fc788a5813
Grantas Gadliauskas <grantas0612@gmail.com>
2020-05-11 13:39:33 +0300

    add client url variable in common frontend

--- frontend/commonFrontend/src/main/kotlin/Main.kt
+++ frontend/commonFrontend/src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ import org.ktugrades.common.MarkInfoResponse
 
 const val CACHE_USERNAME = "username"
 const val DATA_CACHE = "dataCache"
+
 const val SERVER_URL = ""
+const val CLIENT_URL = ""
 
 inline fun jsObject(init: dynamic.() -> Unit): dynamic {
     val o = js("{}")
